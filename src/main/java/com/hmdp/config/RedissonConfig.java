ea8942bbc414d48e013d40fe98c667a3f0a74544
punkdog <1084753928@qq.com>
2024-12-16 17:51:13 +0800

    对yaml文件进行了一个分类处理，分为生产和开发两个yaml文件

--- src/main/java/com/hmdp/config/RedissonConfig.java
+++ src/main/java/com/hmdp/config/RedissonConfig.java
@@ -3,16 +3,27 @@ package com.hmdp.config;
 import org.redisson.Redisson;
 import org.redisson.api.RedissonClient;
 import org.redisson.config.Config;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class RedissonConfig {
+
+    @Value("${spring.redis.host}")
+    private String redisHost;
+
+    @Value("${spring.redis.port}")
+    private int redisPort;
+
+
+
     @Bean
     public RedissonClient redissonClient() {
         Config config = new Config();
-        config.useSingleServer().setAddress("redis://192.168.3.8:6379").setPassword("123456");
-        //config.useSingleServer().setAddress("redis://192.168.3.26:6379").setPassword("123456");
+        config.useSingleServer()
+                .setAddress("redis://" + redisHost + ":" + redisPort);
+
         return Redisson.create(config);
     }
 }
