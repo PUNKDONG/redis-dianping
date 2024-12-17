7c6b8f71ec9e85e34b82883f1b20b0e709e40a52
punkdog <1084753928@qq.com>
2024-12-17 21:34:24 +0800

    完成maven父子工程改进，拆分为主项目和订单处理项目

---
+++ OrderComsumer/src/main/java/com/hmdp/ordercomsumer/OrderComsumerApplication.java
@@ -0,0 +1,13 @@
+package com.hmdp.ordercomsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderComsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OrderComsumerApplication.class, args);
+    }
+
+}
